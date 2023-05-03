6c21d8b73dc2a57530cd913368fdf29f0bb15601
marcley <marcleyrodrigues6@gmail.com>
2023-05-03 18:34:43 -0300

    marcley

---
+++ strypt.js
@@ -0,0 +1,17 @@
+const shareButtons = document.querySelectorAll('.tile-share-button')
+console.log(shareButtons)
+
+async function copyText(e) {
+    e.preventDefault()
+    const link = this.getAttribute('link')
+    console.log(link)
+    try {
+        await navigator.clipboard.writeText(link)
+        alert("Texto Copiado: " + link)
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+shareButtons.forEach(shareButton =>
+    shareButton.addEventListener('click', copyText))
